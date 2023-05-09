b9894c3c0fdacd305670f68db4ad8c23fa228611
hanaa.s.hammad@gmail.com <34770828+hanouma@users.noreply.github.com>
2023-05-09 09:25:37 +0300

    Add files via upload

---
+++ stac_tutoriel.py
@@ -0,0 +1,81 @@
+import dask.distributed
+import folium
+import folium.plugins
+import geopandas as gpd
+import shapely.geometry
+from IPython.display import HTML, display
+from pystac_client import Client
+
+from odc.stac import configure_rio, stac_load
+pip install rio-cogeo
+
+
+def convert_bounds(bbox, invert_y=False):
+    """
+    Helper method for changing bounding box representation to leaflet notation
+
+    ``(lon1, lat1, lon2, lat2) -> ((lat1, lon1), (lat2, lon2))``
+    """
+    x1, y1, x2, y2 = bbox
+    if invert_y:
+        y1, y2 = y2, y1
+    return ((y1, x1), (y2, x2))
+
+
+cfg = {
+    "sentinel-s2-l2a-cogs": {
+        "assets": {
+            "*": {"data_type": "uint16", "nodata": 0},
+            "SCL": {"data_type": "uint8", "nodata": 0},
+            "visual": {"data_type": "uint8", "nodata": 0},
+        },
+        "aliases": {"red": "B04", "green": "B03", "blue": "B02"},
+    },
+    "*": {"warnings": "ignore"},
+}
+
+client = dask.distributed.Client()
+configure_rio(cloud_defaults=True, aws={"aws_unsigned": True}, client=client)
+display(client)
+
+catalog = Client.open("https://planetarycomputer.microsoft.com/api/stac/v1")
+
+query = catalog.search(
+    collections=["sentinel-2-l2a"],
+    datetime="2019-06",
+    query={"s2:mgrs_tile": dict(eq="06VVN")},
+)
+
+items = list(query.get_items())
+print(f"Found: {len(items):d} datasets")
+
+resolution = 10
+SHRINK = 4
+if client.cluster.workers[0].memory_limit < dask.utils.parse_bytes("4G"):
+    SHRINK = 8  # running on Binder with 2Gb RAM
+
+if SHRINK > 1:
+    resolution = resolution * SHRINK
+
+xx = stac_load(
+    items,
+    chunks={"x": 2048, "y": 2048},
+    stac_cfg=cfg,
+    patch_url=pc.sign,
+    resolution=resolution,
+)
+
+print(f"Bands: {','.join(list(xx.data_vars))}")
+display(xx)
+xx = stac_load(
+    items,
+    bands=["red", "green", "blue", "nir", "SCL"],
+    resolution=resolution,
+    chunks={"x": 2048, "y": 2048},
+    stac_cfg=cfg,
+    patch_url=pc.sign,
+)
+
+print(f"Bands: {','.join(list(xx.data_vars))}")
+display(xx)
+
